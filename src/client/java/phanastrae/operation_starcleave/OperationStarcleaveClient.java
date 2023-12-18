19f42a8de98b62f93164fee1a60adf04e009dfc3
Phanastrae <111198528+Phanastrae@users.noreply.github.com>
2023-12-18 22:53:00 +0000

    Added Fabric template files

---
+++ src/client/java/phanastrae/operation_starcleave/OperationStarcleaveClient.java
@@ -0,0 +1,10 @@
+package phanastrae.operation_starcleave;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OperationStarcleaveClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/phanastrae/operation_starcleave/OperationStarcleaveDataGenerator.java
@@ -0,0 +1,11 @@
+package phanastrae.operation_starcleave;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class OperationStarcleaveDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
