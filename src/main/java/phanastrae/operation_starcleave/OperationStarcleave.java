c91313bb5cf50443fddfaf16ca12fdf29ab65be3
phanastrae <phanastrae@gmail.com>
2023-12-19 02:09:07 +0000

    Swapped dashes with underscores

--- src/main/java/phanastrae/operation_starcleave/OperationStarcleave.java
+++ src/main/java/phanastrae/operation_starcleave/OperationStarcleave.java
@@ -2,11 +2,16 @@ package phanastrae.operation_starcleave;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class OperationStarcleave implements ModInitializer {
-    public static final Logger LOGGER = LoggerFactory.getLogger("operation-starcleave");
+    public static final Logger LOGGER = LoggerFactory.getLogger("operation_starcleave");
+
+    public static Identifier id(String path) {
+    	return new Identifier("operation_starcleave", path);
+	}
 
 	@Override
 	public void onInitialize() {
