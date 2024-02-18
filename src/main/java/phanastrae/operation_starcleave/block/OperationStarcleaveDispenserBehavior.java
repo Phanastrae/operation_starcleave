416cbeaf55cd6bef8d7551321ebaf64fc185a104
phanastrae <phanastrae@gmail.com>
2024-02-18 15:18:58 +0000

    Added missing dispenser behaviour for some projectiles

--- src/main/java/phanastrae/operation_starcleave/block/OperationStarcleaveDispenserBehavior.java
+++ src/main/java/phanastrae/operation_starcleave/block/OperationStarcleaveDispenserBehavior.java
@@ -1,21 +1,31 @@
 package phanastrae.operation_starcleave.block;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.DispenserBlock;
+import net.minecraft.block.*;
 import net.minecraft.block.dispenser.DispenserBehavior;
 import net.minecraft.block.dispenser.FallibleItemDispenserBehavior;
+import net.minecraft.block.dispenser.ItemDispenserBehavior;
 import net.minecraft.block.dispenser.ProjectileDispenserBehavior;
+import net.minecraft.block.entity.BeehiveBlockEntity;
 import net.minecraft.entity.projectile.ProjectileEntity;
+import net.minecraft.entity.projectile.thrown.PotionEntity;
 import net.minecraft.entity.projectile.thrown.SnowballEntity;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ItemConvertible;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.potion.PotionUtil;
+import net.minecraft.potion.Potions;
+import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.registry.tag.FluidTags;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Util;
 import net.minecraft.util.math.BlockPointer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Position;
 import net.minecraft.world.World;
 import net.minecraft.world.event.GameEvent;
+import phanastrae.operation_starcleave.entity.projectile.FirmamentRejuvenatorEntity;
+import phanastrae.operation_starcleave.entity.projectile.SplashStarbleachEntity;
 import phanastrae.operation_starcleave.entity.projectile.StarbleachedPearlEntity;
 import phanastrae.operation_starcleave.item.OperationStarcleaveItems;
 
@@ -48,6 +58,45 @@ public class OperationStarcleaveDispenserBehavior {
             @Override
             protected StarbleachedPearlEntity createProjectile(World world, Position position, ItemStack stack) {
                 return Util.make(new StarbleachedPearlEntity(world, position.getX(), position.getY(), position.getZ()), entity -> entity.setItem(stack));
+            }
+
+            @Override
+            protected float getForce() {
+                return super.getForce() * 1.5F;
+            }
+        });
+
+        register(OperationStarcleaveItems.SPLASH_STARBLEACH_BOTTLE, new ProjectileDispenserBehavior() {
+            @Override
+            protected ProjectileEntity createProjectile(World world, Position position, ItemStack stack) {
+                return Util.make(new SplashStarbleachEntity(world, position.getX(), position.getY(), position.getZ()),
+                        entity -> {
+                            entity.setItem(stack);
+                            entity.setCanStarbleach(true);
+                        });
+            }
+
+            @Override
+            protected float getVariation() {
+                return super.getVariation() * 0.5F;
+            }
+
+            @Override
+            protected float getForce() {
+                return super.getForce() * 1.25F;
+            }
+        });
+
+        register(OperationStarcleaveItems.FIRMAMENT_REJUVENATOR, new ProjectileDispenserBehavior() {
+            @Override
+            protected ProjectileEntity createProjectile(World world, Position position, ItemStack stack) {
+                return Util.make(new FirmamentRejuvenatorEntity(world, position.getX(), position.getY(), position.getZ()),
+                        entity -> entity.setItem(stack));
+            }
+
+            @Override
+            protected float getVariation() {
+                return super.getVariation() * 0.1F;
             }
 
             @Override
