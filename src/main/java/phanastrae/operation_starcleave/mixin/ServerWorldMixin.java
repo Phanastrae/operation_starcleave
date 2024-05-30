a3c7463eeedcc0c80d30314ba0480ef4a4339190
phanastrae <phanastrae@gmail.com>
2024-05-30 10:58:18 +0100

    Damaged Firmament now blocks rain and snow

--- src/main/java/phanastrae/operation_starcleave/mixin/ServerWorldMixin.java
+++ src/main/java/phanastrae/operation_starcleave/mixin/ServerWorldMixin.java
@@ -1,9 +1,12 @@
 package phanastrae.operation_starcleave.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.WorldGenerationProgressListener;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.random.RandomSequencesState;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.WorldChunk;
@@ -14,8 +17,8 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import phanastrae.operation_starcleave.world.firmament.FirmamentHolder;
 import phanastrae.operation_starcleave.world.firmament.Firmament;
+import phanastrae.operation_starcleave.world.firmament.FirmamentHolder;
 import phanastrae.operation_starcleave.world.firmament.ServerFirmamentRegionManager;
 import phanastrae.operation_starcleave.world.starbleach.Starbleach;
 
@@ -41,5 +44,17 @@ public class ServerWorldMixin implements FirmamentHolder {
     @Inject(method = "tickChunk", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/profiler/Profiler;pop()V", shift = At.Shift.BEFORE))
     private void operation_starcleave$starbleachChunk(WorldChunk chunk, int randomTickSpeed, CallbackInfo ci) {
         Starbleach.starbleachChunk((ServerWorld)(Object)this, chunk, randomTickSpeed);
+    }
+
+    // Stop Precipitation beneath damaged firmament
+    @Inject(method = "tickIceAndSnow", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/server/world/ServerWorld;getTopPosition(Lnet/minecraft/world/Heightmap$Type;Lnet/minecraft/util/math/BlockPos;)Lnet/minecraft/util/math/BlockPos;", shift = At.Shift.AFTER))
+    private void operation_starcleave$blockIceAndSnowTick(CallbackInfo ci, @Local(ordinal = 1) LocalRef<BlockPos> refTopPosition) {
+        Firmament firmament = this.operation_starcleave$getFirmament();
+
+        BlockPos topPos = refTopPosition.get();
+        int damage = firmament.getDamage(topPos.getX(), topPos.getZ());
+        if(damage >= 5) {
+            refTopPosition.set(new BlockPos(topPos.getX(), firmament.getY(), topPos.getZ()));
+        }
     }
 }
--- src/main/java/phanastrae/operation_starcleave/mixin/WorldMixin.java
+++ src/main/java/phanastrae/operation_starcleave/mixin/WorldMixin.java
@@ -3,13 +3,16 @@ package phanastrae.operation_starcleave.mixin;
 import net.minecraft.registry.DynamicRegistryManager;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.MutableWorldProperties;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import phanastrae.operation_starcleave.duck.WorldDuck;
+import phanastrae.operation_starcleave.world.firmament.Firmament;
 
 import java.util.function.Supplier;
 
@@ -30,5 +33,18 @@ public class WorldMixin implements WorldDuck {
     @Override
     public int operation_starcleave$getCleavingFlashTicksLeft() {
         return this.operation_starcleave$cleavingFlashTicksLeft;
+    }
+
+    // Stop Precipitation beneath damaged firmament
+    @Inject(method = "hasRain", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;getBiome(Lnet/minecraft/util/math/BlockPos;)Lnet/minecraft/registry/entry/RegistryEntry;", shift = At.Shift.BEFORE), cancellable = true)
+    private void operation_starcleave$blockRain(BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
+        World world = (World)(Object)this;
+        Firmament firmament = Firmament.fromWorld(world);
+        if(firmament == null) return;
+
+        int damage = firmament.getDamage(pos.getX(), pos.getZ());
+        if(damage >= 5 && firmament.getY() > pos.getY()) {
+            cir.setReturnValue(false);
+        }
     }
 }
