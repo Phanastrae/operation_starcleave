0865fb2a2008a2bb47973f3154096fb5dc83dc3a
Phanastrae <111198528+Phanastrae@users.noreply.github.com>
2025-09-26 02:19:28 +0100

    feat: increase hollowed sac drop rates

--- fabric/src/main/java/phanastrae/operation_starcleave/fabric/data/EntityLootTableProvider.java
+++ fabric/src/main/java/phanastrae/operation_starcleave/fabric/data/EntityLootTableProvider.java
@@ -8,28 +8,45 @@ import net.minecraft.world.item.Items;
 import net.minecraft.world.level.storage.loot.LootPool;
 import net.minecraft.world.level.storage.loot.LootTable;
 import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.functions.EnchantedCountIncreaseFunction;
+import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
-import net.minecraft.world.level.storage.loot.providers.number.BinomialDistributionGenerator;
 import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
 import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator;
 import phanastrae.operation_starcleave.entity.OperationStarcleaveEntityTypes;
 import phanastrae.operation_starcleave.item.OperationStarcleaveItems;
 
+import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.BiConsumer;
 
 public class EntityLootTableProvider extends SimpleFabricLootTableProvider {
+
+    private final CompletableFuture<HolderLookup.Provider> registryLookup;
+
     public EntityLootTableProvider(FabricDataOutput output, CompletableFuture<HolderLookup.Provider> registryLookup) {
         super(output, registryLookup, LootContextParamSets.ENTITY);
+        this.registryLookup = registryLookup;
     }
 
     @Override
     public void generate(BiConsumer<ResourceKey<LootTable>, LootTable.Builder> lootTableBiConsumer) {
+        // generate() should be getting run inside of run() which should have got the registryLookup by the time this gets run
+        HolderLookup.Provider registries = this.registryLookup.getNow(null);
+        Objects.requireNonNull(registries);
+
         lootTableBiConsumer.accept(OperationStarcleaveEntityTypes.STARCLEAVER_GOLEM.getDefaultLootTable(), LootTable.lootTable()
-                .withPool(LootPool.lootPool().setRolls(ConstantValue.exactly(1))
-                        .add(LootItem.lootTableItem(Items.GOLD_NUGGET))));
+                .withPool(LootPool.lootPool().setRolls(ConstantValue.exactly(1.0F))
+                        .add(LootItem.lootTableItem(Items.GOLD_NUGGET)))
+        );
+
         lootTableBiConsumer.accept(OperationStarcleaveEntityTypes.SUBCAELIC_TORPEDO.getDefaultLootTable(), LootTable.lootTable()
-                .withPool(LootPool.lootPool().setRolls(BinomialDistributionGenerator.binomial(1, 0.3F))
-                        .add(LootItem.lootTableItem(OperationStarcleaveItems.HOLLOWED_SAC))));
+                .withPool(LootPool.lootPool().setRolls(ConstantValue.exactly(1.0F))
+                        .add(LootItem.lootTableItem(OperationStarcleaveItems.HOLLOWED_SAC)
+                                .apply(SetItemCountFunction.setCount(UniformGenerator.between(1.0F, 2.0F)))
+                                .apply(EnchantedCountIncreaseFunction.lootingMultiplier(registries, UniformGenerator.between(0.0F, 1.0F)))
+                        )
+                )
+        );
     }
 }
