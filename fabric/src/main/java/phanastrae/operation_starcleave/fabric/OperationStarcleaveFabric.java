042623f66497cf2f914386109815707e65332c12
Phanastrae <111198528+Phanastrae@users.noreply.github.com>
2026-01-15 01:41:03 +0000

    feat: Make plasma bucket break when used on fluid storages (Fabric)

--- fabric/src/main/java/phanastrae/operation_starcleave/fabric/OperationStarcleaveFabric.java
+++ fabric/src/main/java/phanastrae/operation_starcleave/fabric/OperationStarcleaveFabric.java
@@ -9,6 +9,10 @@ import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
 import net.fabricmc.fabric.api.registry.StrippableBlockRegistry;
 import net.fabricmc.fabric.api.registry.TillableBlockRegistry;
+import net.fabricmc.fabric.api.transfer.v1.fluid.FluidConstants;
+import net.fabricmc.fabric.api.transfer.v1.fluid.FluidStorage;
+import net.fabricmc.fabric.api.transfer.v1.fluid.FluidVariant;
+import net.fabricmc.fabric.api.transfer.v1.fluid.base.FullItemFluidStorage;
 import net.minecraft.core.Registry;
 import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.codec.StreamCodec;
@@ -18,12 +22,15 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.ItemLike;
 import phanastrae.operation_starcleave.OperationStarcleave;
 import phanastrae.operation_starcleave.block.OperationStarcleaveToolActions;
 import phanastrae.operation_starcleave.entity.OperationStarcleaveEntityTypes;
 import phanastrae.operation_starcleave.fabric.fluid.OperationStarcleaveFluidVariantAttributes;
+import phanastrae.operation_starcleave.fluid.OperationStarcleaveFluids;
 import phanastrae.operation_starcleave.item.OperationStarcleaveCreativeModeTabs;
+import phanastrae.operation_starcleave.item.OperationStarcleaveItems;
 import phanastrae.operation_starcleave.network.packet.OperationStarcleavePayloads;
 
 import java.util.Collection;
@@ -67,6 +74,9 @@ public class OperationStarcleaveFabric implements ModInitializer {
 
         // setup tilling
         OperationStarcleaveToolActions.TILLABLES.forEach(triple -> TillableBlockRegistry.register(triple.getLeft(), triple.getMiddle(), triple.getRight()));
+
+        // setup fluid storages
+        setupFluidStorages();
 
 
         // world tick start
@@ -165,5 +175,11 @@ public class OperationStarcleaveFabric implements ModInitializer {
                 ServerPlayNetworking.registerGlobalReceiver(id, (payload, context) -> serverCallback.accept(payload, context.player()));
             }
         });
+    }
+
+    public void setupFluidStorages() {
+        // TODO replace iron nugget with a different item, and make consistent across all interactions
+        // specifically do NOT use the combinedItemApiProvider here, to make sure that the default bucket behaviour does NOT get run.
+        FluidStorage.ITEM.registerForItems(((itemStack, context) -> new FullItemFluidStorage(context, Items.IRON_NUGGET, FluidVariant.of(OperationStarcleaveFluids.PETRICHORIC_PLASMA), FluidConstants.BUCKET)), OperationStarcleaveItems.PETRICHORIC_PLASMA_BUCKET);
     }
 }
---
+++ fabric/src/main/java/phanastrae/operation_starcleave/fabric/mixin/FluidStorageUtilMixin.java
@@ -0,0 +1,34 @@
+package phanastrae.operation_starcleave.fabric.mixin;
+
+import com.llamalad7.mixinextras.sugar.Local;
+import net.fabricmc.fabric.api.transfer.v1.fluid.FluidStorageUtil;
+import net.fabricmc.fabric.api.transfer.v1.fluid.FluidVariant;
+import net.fabricmc.fabric.api.transfer.v1.storage.Storage;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import phanastrae.operation_starcleave.item.OperationStarcleaveItems;
+
+@Mixin(FluidStorageUtil.class)
+public class FluidStorageUtilMixin {
+    // the forbidden "mixing into Fabric" technique
+    @Inject(method = "moveWithSound", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;playSound(Lnet/minecraft/world/entity/player/Player;DDDLnet/minecraft/sounds/SoundEvent;Lnet/minecraft/sounds/SoundSource;FF)V"))
+    private static void operation_starcleave$playBucketBreakingSounds(Storage<FluidVariant> from, Storage<FluidVariant> _to, Player player, boolean fill, Item handItem, CallbackInfoReturnable<Boolean> cir, @Local FluidVariant resource) {
+        // if draining a plasma bucket, play the sound
+        if (!fill && handItem == OperationStarcleaveItems.PETRICHORIC_PLASMA_BUCKET && !player.getAbilities().instabuild) {
+            player.level().playSound(
+                    null,
+                    player.getX(), player.getEyeY(), player.getZ(),
+                    Items.IRON_PICKAXE.getBreakingSound(),
+                    SoundSource.PLAYERS,
+                    0.8F,
+                    1.0F
+            );
+        }
+    }
+}
