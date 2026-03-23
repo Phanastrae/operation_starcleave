386bd17069eaa28af3ddf10b6a333910579e3276
Phanastrae <111198528+Phanastrae@users.noreply.github.com>
2026-03-23 02:03:38 +0000

    feat: Adjust Imbued Starbleached Tiles particles
    - Spawn more gradually rather than in bursts
    - Reduce count
    - Replace some with Swirls
    - Adjust spawn positions to only spawn nearer the middle of a face

--- common/src/main/java/phanastrae/operation_starcleave/block/ImbuedStarbleachedTilesBlock.java
+++ common/src/main/java/phanastrae/operation_starcleave/block/ImbuedStarbleachedTilesBlock.java
@@ -3,6 +3,7 @@ package phanastrae.operation_starcleave.block;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.Vec3i;
+import net.minecraft.core.particles.SimpleParticleType;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
@@ -16,23 +17,31 @@ public class ImbuedStarbleachedTilesBlock extends Block {
     }
 
     @Override
-    public void animateTick(BlockState state, Level world, BlockPos pos, RandomSource random) {
-        if(random.nextInt(5) != 0) return;
-
-        for(Direction direction : Direction.values()) {
+    public void animateTick(BlockState state, Level level, BlockPos pos, RandomSource random) {
+        for (Direction direction : Direction.values()) {
             Vec3i v = direction.getNormal();
-            if (world.getBlockState(pos.offset(v)).canBeReplaced()) {
-                double x = pos.getX() + 0.5 + 0.5 * v.getX();
-                double y = pos.getY() + 0.5 + 0.5 * v.getY();
-                double z = pos.getZ() + 0.5 + 0.5 * v.getZ();
-                for(int k = 0; k < 25; k++) {
-                    world.addParticle(OperationStarcleaveParticleTypes.FIRMAMENT_GLIMMER,
-                            x + (v.getX() == 0 ? random.nextFloat() - 0.5 : 0),
-                            y + (v.getY() == 0 ? random.nextFloat() - 0.5 : 0),
-                            z + (v.getZ() == 0 ? random.nextFloat() - 0.5 : 0),
-                            v.getX() * 0.05 + random.nextFloat() * 0.05 - 0.025,
-                            v.getY() * 0.05 + random.nextFloat() * 0.05 - 0.025,
-                            v.getZ() * 0.05 + random.nextFloat() * 0.05 - 0.025);
+            if (level.getBlockState(pos.offset(v)).canBeReplaced()) {
+                int vx = v.getX();
+                int vy = v.getY();
+                int vz = v.getZ();
+
+                for (int k = 0; k < 2; k++) {
+                    SimpleParticleType type;
+                    if (random.nextInt(21) == 0) {
+                        type = OperationStarcleaveParticleTypes.STARBLEACH_SWIRL;
+                    } else {
+                        type = OperationStarcleaveParticleTypes.FIRMAMENT_GLIMMER;
+                    }
+
+                    double x = pos.getX() + (vx == 0 ? 0.15 + 0.7 * random.nextFloat() : 0.5 + 0.5 * vx);
+                    double y = pos.getY() + (vy == 0 ? 0.15 + 0.7 * random.nextFloat() : 0.5 + 0.5 * vy);
+                    double z = pos.getZ() + (vz == 0 ? 0.15 + 0.7 * random.nextFloat() : 0.5 + 0.5 * vz);
+
+                    level.addParticle(type, x, y, z,
+                            (vx + random.nextFloat() - 0.5) * 0.05,
+                            (vy + random.nextFloat() - 0.5) * 0.05,
+                            (vz + random.nextFloat() - 0.5) * 0.05
+                    );
                 }
             }
         }
