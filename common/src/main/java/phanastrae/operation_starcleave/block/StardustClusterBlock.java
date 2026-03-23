e90e98686a52d9bb7a6d708b0b39795fd385107f
Phanastrae <111198528+Phanastrae@users.noreply.github.com>
2026-03-23 02:27:25 +0000

    - feat: Adjust Stardust Cluster
    - Reduce hitbox size a bit
    - Adjust particles to include Stardust Swirls and Glimmer Smoke, and reduce overall count
    - Fix Stardust Clusters not being Waterlogged if placed underwater

--- common/src/main/java/phanastrae/operation_starcleave/block/StardustClusterBlock.java
+++ common/src/main/java/phanastrae/operation_starcleave/block/StardustClusterBlock.java
@@ -3,7 +3,9 @@ package phanastrae.operation_starcleave.block;
 import com.mojang.serialization.MapCodec;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.core.particles.SimpleParticleType;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
@@ -19,12 +21,14 @@ import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
+import org.jetbrains.annotations.Nullable;
 import phanastrae.operation_starcleave.item.OperationStarcleaveItems;
 import phanastrae.operation_starcleave.particle.OperationStarcleaveParticleTypes;
 
 public class StardustClusterBlock extends Block implements SimpleWaterloggedBlock {
     public static final MapCodec<StardustClusterBlock> CODEC = simpleCodec(StardustClusterBlock::new);
     public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
+    public static final VoxelShape SHAPE = Block.box(3, 3, 3, 13, 13, 13);
 
     @Override
     public MapCodec<StardustClusterBlock> codec() {
@@ -33,7 +37,7 @@ public class StardustClusterBlock extends Block implements SimpleWaterloggedBloc
 
     public StardustClusterBlock(Properties settings) {
         super(settings);
-        this.registerDefaultState(this.stateDefinition.any().setValue(WATERLOGGED, Boolean.valueOf(false)));
+        this.registerDefaultState(this.stateDefinition.any().setValue(WATERLOGGED, false));
     }
 
     @Override
@@ -42,8 +46,13 @@ public class StardustClusterBlock extends Block implements SimpleWaterloggedBloc
     }
 
     @Override
+    public FluidState getFluidState(BlockState state) {
+        return state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
+    }
+
+    @Override
     public VoxelShape getShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
-        return context.isHoldingItem(OperationStarcleaveItems.STARDUST_CLUSTER) ? Shapes.block() : Shapes.empty();
+        return context.isHoldingItem(OperationStarcleaveItems.STARDUST_CLUSTER) ? SHAPE : Shapes.empty();
     }
 
     @Override
@@ -61,6 +70,13 @@ public class StardustClusterBlock extends Block implements SimpleWaterloggedBloc
         return 1.0F;
     }
 
+    @Nullable
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return this.defaultBlockState()
+                .setValue(WATERLOGGED, context.getLevel().getFluidState(context.getClickedPos()).getType() == Fluids.WATER);
+    }
+
     @Override
     public BlockState updateShape(
             BlockState state, Direction direction, BlockState neighborState, LevelAccessor world, BlockPos pos, BlockPos neighborPos
@@ -73,23 +89,38 @@ public class StardustClusterBlock extends Block implements SimpleWaterloggedBloc
     }
 
     @Override
-    public FluidState getFluidState(BlockState state) {
-        return state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
-    }
-
-    @Override
     public void animateTick(BlockState state, Level world, BlockPos pos, RandomSource random) {
         double x = pos.getX() + 0.5;
         double y = pos.getY() + 0.5;
         double z = pos.getZ() + 0.5;
-        for(int k = 0; k < 40; k++) {
-            world.addParticle(OperationStarcleaveParticleTypes.FIRMAMENT_GLIMMER,
-                    x + random.nextFloat() * 0.25 - 0.125,
-                    y + random.nextFloat() * 0.25 - 0.125,
-                    z + random.nextFloat() * 0.25 - 0.125,
-                    random.nextFloat() * 0.05 - 0.025,
-                    random.nextFloat() * 0.05 - 0.025,
-                    random.nextFloat() * 0.05 - 0.025);
+
+        for (int k = 0; k < 10 + random.nextInt(10); k++) {
+            SimpleParticleType type;
+            double speed;
+            double range;
+            int r = random.nextInt(12);
+            if (r == 0) {
+                type = OperationStarcleaveParticleTypes.STARBLEACH_SWIRL;
+                speed = 0.015;
+                range = 0.25;
+            } else if (r <= 2) {
+                type = OperationStarcleaveParticleTypes.GLIMMER_SMOKE;
+                speed = 0.025;
+                range = 0.35;
+            } else {
+                type = OperationStarcleaveParticleTypes.FIRMAMENT_GLIMMER;
+                speed = 0.05;
+                range = 0.25;
+            }
+
+            world.addParticle(type,
+                    x + (random.nextFloat() - 0.5) * range,
+                    y + (random.nextFloat() - 0.5) * range,
+                    z + (random.nextFloat() - 0.5) * range,
+                    (random.nextFloat() - 0.5) * speed,
+                    (random.nextFloat() - 0.5) * speed,
+                    (random.nextFloat() - 0.5) * speed
+            );
         }
     }
 }
