de304df10de63708cd7ca20a56522326067dc765
Phanastrae <111198528+Phanastrae@users.noreply.github.com>
2026-04-16 19:27:21 +0100

    refactor: Tidy ClientFirmamentRegionManager

--- common/src/main/java/phanastrae/operation_starcleave/client/world/firmament/ClientFirmamentRegionManager.java
+++ common/src/main/java/phanastrae/operation_starcleave/client/world/firmament/ClientFirmamentRegionManager.java
@@ -7,24 +7,25 @@ import phanastrae.operation_starcleave.client.render.firmament.FirmamentTextureS
 import phanastrae.operation_starcleave.world.firmament.Firmament;
 import phanastrae.operation_starcleave.world.firmament.FirmamentRegion;
 import phanastrae.operation_starcleave.world.firmament.FirmamentRegionHolder;
+import phanastrae.operation_starcleave.world.firmament.pos.RegionPos;
 import phanastrae.operation_starcleave.world.firmament.region_manager.FirmamentRegionManager;
 
 import java.util.function.Consumer;
 
 public class ClientFirmamentRegionManager extends FirmamentRegionManager {
 
-    Long2ObjectLinkedOpenHashMap<FirmamentRegionHolder> firmamentRegionHolders = new Long2ObjectLinkedOpenHashMap<>();
+    private final Long2ObjectLinkedOpenHashMap<FirmamentRegionHolder> firmamentRegionHolders = new Long2ObjectLinkedOpenHashMap<>();
+    private final ClientLevel level;
 
-    private final ClientLevel clientWorld;
-    public ClientFirmamentRegionManager(ClientLevel clientWorld) {
-        this.clientWorld = clientWorld;
+    public ClientFirmamentRegionManager(ClientLevel level) {
+        this.level = level;
     }
 
     @Override
     public void forEachRegion(Consumer<FirmamentRegion> method) {
         this.firmamentRegionHolders.forEach((id, firmamentRegionHolder) -> {
             FirmamentRegion firmamentRegion = firmamentRegionHolder.getFirmamentRegion();
-            if(firmamentRegion != null) {
+            if (firmamentRegion != null) {
                 method.accept(firmamentRegion);
             }
         });
@@ -33,7 +34,7 @@ public class ClientFirmamentRegionManager extends FirmamentRegionManager {
     @Nullable
     @Override
     public FirmamentRegion getFirmamentRegion(long id) {
-        if(this.firmamentRegionHolders.containsKey(id)) {
+        if (this.firmamentRegionHolders.containsKey(id)) {
             return this.firmamentRegionHolders.get(id).getFirmamentRegion();
         } else {
             return null;
@@ -45,33 +46,26 @@ public class ClientFirmamentRegionManager extends FirmamentRegionManager {
     }
 
     public FirmamentRegionHolder loadRegion(long id) {
-        if(this.firmamentRegionHolders.containsKey(id)) {
+        if (this.firmamentRegionHolders.containsKey(id)) {
             return firmamentRegionHolders.get(id);
+        } else {
+            RegionPos regionPos = new RegionPos(id);
+            FirmamentRegion firmamentRegion = new FirmamentRegion(Firmament.fromLevel(this.level), regionPos);
+
+            FirmamentRegionHolder firmamentRegionHolder = new FirmamentRegionHolder(firmamentRegion);
+            firmamentRegionHolder.recordAccess();
+
+            this.firmamentRegionHolders.put(id, firmamentRegionHolder);
+            firmamentRegionHolder.setState(FirmamentRegionHolder.FirmamentRegionState.STARTED);
+
+            return firmamentRegionHolder;
         }
-
-        int rx = (int)(id & 4294967295L);
-        int rz = (int)((id >>> 32) & 4294967295L);
-        int x = rx << FirmamentRegion.REGION_SIZE_BITS;
-        int z = rz << FirmamentRegion.REGION_SIZE_BITS;
-
-        FirmamentRegion firmamentRegion = new FirmamentRegion(Firmament.fromLevel(this.clientWorld), x, z);
-
-        FirmamentRegionHolder firmamentRegionHolder = new FirmamentRegionHolder(firmamentRegion);
-        firmamentRegionHolder.recordAccess();
-
-        this.firmamentRegionHolders.put(id, firmamentRegionHolder);
-        firmamentRegionHolder.setState(FirmamentRegionHolder.FirmamentRegionState.STARTED);
-
-        return firmamentRegionHolder;
     }
 
     public void unloadRegion(long id) {
-        if(!this.firmamentRegionHolders.containsKey(id)) {
-            return;
+        if (this.firmamentRegionHolders.containsKey(id)) {
+            this.firmamentRegionHolders.remove(id);
+            FirmamentTextureStorage.getMainInstance().onRegionRemoved(id);
         }
-
-        this.firmamentRegionHolders.remove(id);
-
-        FirmamentTextureStorage.getMainInstance().onRegionRemoved(id);
     }
 }
