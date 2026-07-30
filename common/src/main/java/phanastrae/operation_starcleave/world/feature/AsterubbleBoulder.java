49e54806ed3ca8734383bbd393417c1713c312e7
Phanastrae <111198528+Phanastrae@users.noreply.github.com>
2026-07-30 03:25:02 +0100

    Convert dirt/sediment blocks next to Asterubble Boulders

--- common/src/main/java/phanastrae/operation_starcleave/world/feature/AsterubbleBoulder.java
+++ common/src/main/java/phanastrae/operation_starcleave/world/feature/AsterubbleBoulder.java
@@ -2,13 +2,19 @@ package phanastrae.operation_starcleave.world.feature;
 
 import com.mojang.serialization.Codec;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.WorldGenLevel;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
 import net.minecraft.world.level.levelgen.feature.configurations.BlockStateConfiguration;
+import phanastrae.operation_starcleave.block.OperationStarcleaveBlocks;
 import phanastrae.operation_starcleave.block.tag.OperationStarcleaveBlockTags;
+
+import static phanastrae.operation_starcleave.block.OperationStarcleaveBlocks.STELLARUBBLE_MIX;
 
 public class AsterubbleBoulder extends Feature<BlockStateConfiguration> {
     public AsterubbleBoulder(Codec<BlockStateConfiguration> config) {
@@ -38,6 +44,13 @@ public class AsterubbleBoulder extends Feature<BlockStateConfiguration> {
             for (BlockPos pos : BlockPos.betweenClosed(centerPos.offset(-xSize, -ySize, -zSize), centerPos.offset(xSize, ySize, zSize))) {
                 if (pos.distSqr(centerPos) <= f * f && canReplace(level.getBlockState(pos))) {
                     level.setBlock(pos, config.state, 3);
+
+                    for (int j = 0; j < 4; j++) {
+                        Direction direction = Direction.from2DDataValue(j);
+                        if (random.nextFloat() < 0.6F) {
+                            tryToRubbleify(level, random, pos.offset(direction.getNormal()));
+                        }
+                    }
                 }
             }
 
@@ -49,5 +62,17 @@ public class AsterubbleBoulder extends Feature<BlockStateConfiguration> {
 
     public boolean canReplace(BlockState state) {
         return state.canBeReplaced() || state.is(OperationStarcleaveBlockTags.ASTERUBBLE_BOULDER_REPLACEABLE);
+    }
+
+    public void tryToRubbleify(WorldGenLevel level, RandomSource random, BlockPos pos) {
+        BlockState adjState = level.getBlockState(pos);
+
+        if (adjState.is(Blocks.DIRT) || adjState.is(Blocks.GRASS_BLOCK) || adjState.is(Blocks.PODZOL) || adjState.is(Blocks.MYCELIUM)) {
+            Block block = random.nextFloat() < 0.6F ? Blocks.DIRT : Blocks.COARSE_DIRT;
+            level.setBlock(pos, block.defaultBlockState(), 3);
+        } else if (adjState.is(OperationStarcleaveBlocks.STELLAR_SEDIMENT) || adjState.is(OperationStarcleaveBlocks.HOLY_MOSS) || adjState.is(OperationStarcleaveBlocks.STELLAR_MULCH)) {
+            Block block = random.nextFloat() < 0.6F ? OperationStarcleaveBlocks.STELLAR_SEDIMENT : STELLARUBBLE_MIX;
+            level.setBlock(pos, block.defaultBlockState(), 3);
+        }
     }
 }
